24aacde177a28898dc1996e46605b565f8d43eda
HagaiLevHacohen <levhacohen@mail.tau.ac.il>
2025-11-27 19:46:40 +0200

    Added an error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router';
 import styles from '../styles/ErrorPage.module.css';
 
 function ErrorPage() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate();
 
   return (
-    <>
-
-    </>
-  )
+    <div className={styles.container}>
+      <h1>404</h1>
+      <h2>Oops! Page not found.</h2>
+      <p>The page you are looking for doesn't exist.</p>
+      
+      <button onClick={() => navigate('/')}>
+        Go Home
+      </button>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
